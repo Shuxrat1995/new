2f19bc802066c91295a97203ce1587900b4bd83b
Shuxrat <shuxratturaev96@mail.com>
2024-08-09 18:37:52 +0500

    new

---
+++ new.js
@@ -0,0 +1,8 @@
+var X = +prompt ('Введите первое число')
+alert (X)
+var Y = +prompt ('Введите второе число')
+alert (Y)
+var Z = +prompt ('Введите третье число')
+alert (Z)
+
+console.log ('Среднее арифметическое :' + (X + Y + Z)  / 3);
